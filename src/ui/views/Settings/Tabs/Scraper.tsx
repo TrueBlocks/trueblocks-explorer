b4b47f9bf6ae675576e3bafbc957caa09b9dee30
tjayrush <jrush@quickblocks.io>
2021-06-05 18:39:04 -0400

    Working on scraper toggles.

--- src/ui/views/Settings/Tabs/Scraper.tsx
+++ src/ui/views/Settings/Tabs/Scraper.tsx
@@ -4,25 +4,27 @@ import React, { useState } from 'react';
 import useGlobalState from '../../../state';
 
 export const Scraper = () => {
-  const [scraperOn1, setScraperOn1] = useState(true);
-  const [scraperOn2, setScraperOn2] = useState(true);
+  const [idxScraperOn, setIndexScraper] = useState(true);
+  const [monScraperOn, setMonitorScraper] = useState(true);
   const { theme } = useGlobalState();
 
   const toggleScraper1 = async () => {
-    await runCommand('scraper');
-    setScraperOn1(!scraperOn1);
+    setIndexScraper(!idxScraperOn);
+    await runCommand('scraper', { tool: 'monitor', mode: idxScraperOn });
   };
 
   const toggleScraper2 = async () => {
-    await runCommand('scraper');
-    setScraperOn2(!scraperOn2);
+    setMonitorScraper(!monScraperOn);
+    await runCommand('scraper', { tool: 'monitor', mode: monScraperOn });
   };
 
   return (
     <>
-      <Button type={scraperOn1 ? 'primary' : 'dashed'} onClick={toggleScraper1}>Toggle Index Scraper</Button>
-      <Button type={scraperOn2 ? 'primary' : 'dashed'} onClick={toggleScraper2}>Toggle Monitor Scraper</Button>
-      <h4 style={{ color: theme ? theme.primaryColor : '#000' }}>scrapers</h4>
+      <Button type={idxScraperOn ? 'primary' : 'dashed'} onClick={toggleScraper1}>Toggle Index Scraper</Button>
+      <Button type={monScraperOn ? 'primary' : 'dashed'} onClick={toggleScraper2}>Toggle Monitor Scraper</Button>
+      <h4 style={{ color: theme ? theme.primaryColor : '#000' }}>
+        scrapers
+      </h4>
     </>
   );
 };

e4ffed162dc9fa691bae0260b24d030e6204dd2c
Dan <dee.aye.en@gmail.com>
2021-06-21 01:18:03 +0300

    Named addresses table is now searchable

--- src/ui/views/Names/Tabs/Addresses.tsx
+++ src/ui/views/Names/Tabs/Addresses.tsx
@@ -1,13 +1,26 @@
+import { SearchOutlined } from '@ant-design/icons';
 import { NamesFilters } from '@components/Filters';
-import { addActionsColumn, addColumn, addFlagColumn, addTagsColumn, BaseTableRows, TableActions } from '@components/Table';
+import {
+  addActionsColumn,
+  addColumn,
+  addFlagColumn,
+  addTagsColumn,
+  BaseTableRows,
+  TableActions,
+} from '@components/Table';
 import { useCommand } from '@hooks/useCommand';
 import { NameType } from '@modules/data/name';
 import { createErrorNotification } from '@modules/error_notification';
+import { Button, Input, Space } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useRef } from 'react';
 import './Addresses.css';
+import Highlighter from 'react-highlight-words';
 
 export const Addresses = () => {
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInputRef = useRef(null);
   const [addresses, loading] = useCommand('names', { expand: true });
   if (addresses.status === 'fail') {
     createErrorNotification({
@@ -16,10 +29,76 @@ export const Addresses = () => {
   }
 
   const getData = useCallback((response) => (response.status === 'fail' ? [] : response.data), []);
+
+  const getColumnSearchProps = (dataIndex: any) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: any) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={searchInputRef}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type='primary'
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size='small'
+            style={{ width: 90 }}>
+            Search
+          </Button>
+          <Button onClick={() => handleReset(clearFilters)} size='small' style={{ width: 90 }}>
+            Reset
+          </Button>
+          <Button
+            type='link'
+            size='small'
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}>
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: any) => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value: any, record: any) =>
+      record[dataIndex] ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()) : '',
+    onFilterDropdownVisibleChange: (visible: any) => {
+      if (visible) {
+        //@ts-ignore
+        setTimeout(() => searchInputRef.current.select(), 100);
+      }
+    },
+  });
+
+  const handleSearch = (selectedKeys: any, confirm: any, dataIndex: any) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: any) => {
+    clearFilters();
+    setSearchText('');
+  };
+
   return (
     <>
       <NamesFilters />
-      <BaseTableRows data={getData(addresses)} columns={addressSchema} loading={loading} />
+      <BaseTableRows
+        data={getData(addresses)}
+        columns={addressSchema.map((item) => {
+          //@ts-ignore
+          return { ...item, ...getColumnSearchProps(item.dataIndex) };
+        })}
+        loading={loading}
+      />
     </>
   );
 };

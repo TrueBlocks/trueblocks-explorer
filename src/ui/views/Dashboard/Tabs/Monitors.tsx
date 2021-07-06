e36c39ea5d336c7f4056863d7e0a6b2bdb09001e
Dan <dee.aye.en@gmail.com>
2021-07-06 16:21:26 +0300

    monitors search

--- src/ui/views/Dashboard/Tabs/Monitors.tsx
+++ src/ui/views/Dashboard/Tabs/Monitors.tsx
@@ -1,14 +1,20 @@
+import { SearchOutlined } from '@ant-design/icons';
 import { addActionsColumn, addColumn, addNumColumn, addTagsColumn, BaseTable, TableActions } from '@components/Table';
 import { useCommand } from '@hooks/useCommand';
 import { createErrorNotification } from '@modules/error_notification';
 import { renderNamedAddress } from '@modules/renderers';
 import { Monitor } from '@modules/types';
+import { Button, Input, Space } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useRef } from 'react';
 import { DashboardAccountsAddressLocation } from '../../../Routes';
 
 export const Monitors = () => {
   const [monitors, loading] = useCommand('status', { mode: 'monitors', details: true });
+  const [searchText, setSearchText] = useState('');
+  const [_, setSearchedColumn] = useState('');
+  const searchInputRef = useRef(null);
+
   if (monitors.status === 'fail') {
     createErrorNotification({
       description: 'Could not fetch monitors',
@@ -19,9 +25,74 @@ export const Monitors = () => {
     return response.status === 'fail' || !response.data[0].caches ? [] : response.data[0].caches[0].items;
   }, []);
 
+  const getColumnSearchProps = (dataIndex: any) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: any) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={searchInputRef}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type='primary'
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size='small'
+            style={{ width: 90 }}>
+            Search
+          </Button>
+          <Button onClick={() => handleReset(clearFilters)} size='small' style={{ width: 90 }}>
+            Reset
+          </Button>
+          <Button
+            type='link'
+            size='small'
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}>
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: any) => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value: any, record: any) =>
+      record[dataIndex] ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()) : '',
+    onFilterDropdownVisibleChange: (visible: any) => {
+      if (visible) {
+        //@ts-ignore
+        setTimeout(() => searchInputRef.current.select(), 100);
+      }
+    },
+  });
+
+  const handleSearch = (selectedKeys: any, confirm: any, dataIndex: any) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: any) => {
+    clearFilters();
+    setSearchText('');
+  };
+
   return (
     <>
-      <BaseTable data={getData(monitors)} columns={monitorSchema} loading={loading} />
+      <BaseTable
+        data={getData(monitors)}
+        columns={monitorSchema.map((item) => {
+          //@ts-ignore
+          return { ...item, ...getColumnSearchProps(item.dataIndex) };
+        })}
+        loading={loading}
+      />
     </>
   );
 };
@@ -69,7 +140,7 @@ const monitorSchema: ColumnsType<Monitor> = [
   }),
   addActionsColumn<Monitor>(
     {
-      title: `<Button>Add</Button>`,
+      title: `Add`,
       dataIndex: '',
       configuration: {
         align: 'right',
